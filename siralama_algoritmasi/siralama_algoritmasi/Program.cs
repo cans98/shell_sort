a5a8c4f5c75cd598185234187b35f0d8cae78d9a
cans98 <53710530+cans98@users.noreply.github.com>
2021-01-11 01:15:25 +0300

    Add files via upload

---
+++ siralama_algoritmasi/siralama_algoritmasi/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace siralama_algoritmasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { -11, 10, 17, 99, 65, 123, -45, 111 };
+
+            int i, j, temp; //shell sorthing algorithm kullanıldı
+            int h = 3; //bosluk sayısı bu degere baglı olarak karsılastırılma yapılır
+            while (h > 0)
+            {
+                for (i = 0; i < numbers.Length; i++) // yer degistirme islemi gerceklesir
+                {
+                    j = i;
+                    temp = numbers[i];
+                    while ((j >= h) && (numbers[j - h] > temp)) // yer degistirme islemi gerceklesir
+                    {
+                        numbers[j] = numbers[j - h];
+                        j = j - h;
+                    }
+                    numbers[j] = temp;
+                }
+                if (h / 2 != 0) //bosluk değeri azaltılır
+                    h = h / 2;
+                else if (h == 1)
+                    h = 0;
+                else
+                    h = 1;
+            }
+        
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+
+            }
+            Console.WriteLine();
+            Console.WriteLine(numbers[0] + " en küçük sayıdır");
+            Console.WriteLine();
+            Console.WriteLine(numbers[numbers.Length - 1] + " en büyük sayıdır");
+            Console.WriteLine();
+
+
+
+            Program pg = new Program();
+            pg.sumAverageElements(numbers, numbers.Length);
+
+            Console.ReadLine();
+     
+            }
+        public void sumAverageElements(int[] arr, int size)
+        {
+
+            int sum = 0;
+            int average = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum += arr[i];
+            }
+            average = sum / size;
+            Console.WriteLine("dizinin ortalaması : " + average);
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ siralama_algoritmasi/siralama_algoritmasi/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("siralama_algoritmasi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("siralama_algoritmasi")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("94a7069c-180d-4990-a89e-a6e71ecae6c8")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
